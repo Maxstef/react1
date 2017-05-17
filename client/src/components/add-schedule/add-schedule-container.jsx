31f17c39bc0b1814ec5675e20c95db79fe9c24f7
Nataliia Romanenko <casstefaks@gmail.com>
2017-05-17 17:43:06 +0300

    begin with select range, comment input mask

--- client/src/components/add-schedule/add-schedule-container.jsx
+++ client/src/components/add-schedule/add-schedule-container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddSchedule from './add-schedule';
 import config from 'react-global-configuration';
+import * as _ from 'lodash';
 
 
 class AddScheduleContainer extends React.Component {
@@ -14,63 +15,99 @@ class AddScheduleContainer extends React.Component {
                     day: 'su',
                     available: false,
                     slots: [],
-                    ranges: 1
+                    timeValue: [{start: ' ', end: ' '}]
                 },
                 {
                     day: 'mo',
                     available: false,
                     slots: [],
-                    ranges: 1
+                    timeValue: [{start: ' ', end: ' '}]
                 },
                 {
                     day: 'tu',
                     available: false,
                     slots: [],
-                    ranges: 1
+                    timeValue: [{start: ' ', end: ' '}]
                 },
                 {
                     day: 'we',
                     available: false,
                     slots: [],
-                    ranges: 1
+                    timeValue: [{start: ' ', end: ' '}]
                 },
                 {
                     day: 'th',
                     available: false,
                     slots: [],
-                    ranges: 1
+                    timeValue: [{start: ' ', end: ' '}]
                 },
                 {
                     day: 'fr',
                     available: false,
                     slots: [],
-                    ranges: 1
+                    timeValue: [{start: ' ', end: ' '}]
                 },
                 {
                     day: 'sa',
                     available: false,
                     slots: [],
-                    ranges: 1
+                    timeValue: [{start: ' ', end: ' '}]
                 }
-            ]
+            ],
+            focus: {day: -1, range: -1, key: ''}
         };
         this.changeDay = this.changeDay.bind(this);
+        this.newRange = this.newRange.bind(this);
+        this.removeRange = this.removeRange.bind(this);
+        this.changeTimeValue = this.changeTimeValue.bind(this);
+        this.setFocus = this.setFocus.bind(this);
     }
 
     changeDay(index){
-        console.log(index);
-        console.log(this.state.days);
         let d = this.state.days;
         d[index].available = !d[index].available;
         this.setState({days: d });
-        console.log(this.state.days);
+    }
+
+    newRange(index){
+        let d = this.state.days;
+        d[index].timeValue.push({start: ' ', end: ' '});
+        this.setState({days: d });
+    }
+
+    removeRange(index){
+        let d = this.state.days;
+        d[index].timeValue.pop();
+        this.setState({days: d });
+    }
+
+    changeTimeValue(day, index, key, value){
+        let d = this.state.days;
+        d[day].timeValue[index][key] = value;
+        this.setState({days: d });
+    }
+
+    setFocus(day, range, key){
+        console.log('here');
+        let d = {
+            day: day,
+            range: range,
+            key: key
+        };
+        this.setState({focus: d });
+        console.log(this.state.focus);
     }
 
     render() {
         return (           
             <AddSchedule slotTimes={this.state.slotTimes}
                          days={this.state.days}
-                         changeDay={this.changeDay} />   
+                         changeDay={this.changeDay}
+                         newRange={this.newRange}
+                         removeRange={this.removeRange}
+                         changeTimeValue={this.changeTimeValue}
+                         focus={this.state.focus}
+                         setFocus={this.setFocus} />   
         );
     }
 }
--- client/src/components/add-schedule/add-schedule.jsx
+++ client/src/components/add-schedule/add-schedule.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Input } from 'reactstrap';
+import { Container, Input, Button } from 'reactstrap';
 import * as _ from 'lodash';
+import InputMask from 'react-input-mask';
 
 
 class AddSchedule extends React.Component {
@@ -8,38 +9,93 @@ class AddSchedule extends React.Component {
     renderAvailableDays(){
         const Day = ({day, ranges, index, checked}) => {
             return (
-                <div className="day-item row">
-                    <div className="available-day-item">
-                        <input type="checkbox" id={day} name={day} checked={checked} onChange={()=>{this.props.changeDay(index)}}/>
-                        <label className={day} htmlFor={day}></label>
+                <div className="day-item row" style={{padding: "15px 0", borderBottom: "1px solid #d1d3d4"}}>
+                    <div className="col-3 col-md-2 col-lg-1">
+                        <div className="available-day-item">
+                            <input type="checkbox" id={day} name={day} checked={checked} onChange={()=>{this.props.changeDay(index)}}/>
+                            <label className={day} htmlFor={day}></label>
+                        </div>
+                        <div>
+                            {checked && ranges < 6 && 
+                                <Button style={{marginLeft: "6px"}} onClick={()=>{this.props.newRange(index)}} type="button" color="success">
+                                    <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                                </Button>
+                            }
+                            {checked && ranges > 1 && 
+                                <Button style={{marginLeft: "6px"}} onClick={() => { this.props.removeRange(index) }} type="button" color="danger" >
+                                    <i className="fa fa-trash" aria-hidden="true"></i>
+                                </Button>
+                            }
+                        </div>
                     </div>
-                    {checked && this.renderTimeRange(day, ranges)}
+                    <div className="col-9 col-md-10 col-lg-11 row">
+                        {checked && this.renderTimeRange(day, ranges, index)}
+                    </div>
                 </div>
             );
         };
         return this.props.days.map((day, index) => (
-            <Day key={index} index={index} day={day.day} ranges={day.ranges} checked={day.available}/>
+            <Day key={index} index={index} day={day.day} ranges={day.timeValue.length} checked={day.available}/>
         ));
     }
 
-    renderTimeRange(day, times){
+    renderTimeRange(day, times, dayIndex){
         const Range = ({index}) => {
             return (
-                <div className="available-hours-item col-4 row">
-                    <div className="col-5">
-                        <Input id={"from-" + day + "-" + index} type="text"/>
+                <div className="available-hours-item col-12 col-sm-12 col-md-9 col-lg-6 col-xl-4  row">
+                    <div className="col-12 col-sm-5">
+                        <Input type="select" id={"from-" + day + "-" + index} name={"from-" + day + "-" + index}>
+                            {this.renderOptions()}
+                        </Input>
+                        {/*
+                        <InputMask className="form-control" mask="12:34" onFocus={() => {this.props.setFocus(dayIndex, index, 'start')}}
+                        autoFocus={(this.props.focus.day == dayIndex && this.props.focus.range == index && this.props.focus.key == 'start')?true:false}
+                        formatChars={{
+                            '1': '[0-2]',
+                            '2': this.props.days[dayIndex].timeValue[index].start.startsWith('2') ? '[0-3]' : '[0-9]',
+                            '3': '[0-5]',
+                            '4': '[0-9]'
+                        }}
+                        value={this.props.days[dayIndex].timeValue[index].start} 
+                        id={"from-" + day + "-" + index} type="text"
+                        onChange={(e)=> {this.props.changeTimeValue(dayIndex, index, 'start', e.target)}}/>*/}
                     </div>
-                    <div className="col-2" style={{textAlign: 'center'}}>
+                    <div className="col-12 col-sm-2" style={{textAlign: 'center'}}>
                         -
                     </div>
-                    <div className="col-5">
-                        <Input id={"to-" + day + "-" + index} type="text"/>
+                    <div className="col-12 col-sm-5">
+                        <Input type="select" id={"to-" + day + "-" + index} name={"to-" + day + "-" + index}>
+                            {this.renderOptions()}
+                        </Input>
+                        {/*
+                        <InputMask className="form-control" mask="12:34" onFocus={() => {this.props.setFocus(dayIndex, index, 'end')}}
+                        autoFocus={(this.props.focus.day == dayIndex && this.props.focus.range == index && this.props.focus.key == 'end')?true:false}
+                        formatChars={{
+                            '1': '[0-2]',
+                            '2': this.props.days[dayIndex].timeValue[index].end.startsWith('2') ? '[0-3]' : '[0-9]',
+                            '3': '[0-5]',
+                            '4': '[0-9]'
+                        }}
+                        value={this.props.days[dayIndex].timeValue[index].end} 
+                        id={"to-" + day + "-" + index} type="text"
+                        onChange={(e)=> {this.props.changeTimeValue(dayIndex, index, 'end', e.target)}}/>*/}
                     </div>
                 </div>
             );
         };
         return Array.from(Array(times)).map((slot, index) => (
             <Range key={index} index={index}/>
+        ));
+    }
+
+    renderOptions(){
+        const Slot = ({slot}) => {
+            return (
+                    <option>{this.props.slotTimes[slot].substr(0, 5)}</option>
+            );
+        };
+        return Array.from(Array(48)).map((slot, index) => (
+            <Slot key={index} slot={index}/>
         ));
     }
 
