f164bd6aa46f74e23ee9046e3b47a63c2af811fe
Maksym Stefanko <casstefaks@gmail.com>
2017-06-16 17:02:37 +0300

    fix

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {};
 
 config.db = "mongodb://max:ch047@ds129442.mlab.com:29442/clinic_ch047";
 config.hashAlgorithm = "sha256";
-config.client = "https://clinic-client-app.herokuapp.com/";
+config.client = "https://clinic-client-app.herokuapp.com";
 
 module.exports = config;
